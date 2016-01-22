0650be2d53a90b0ef89faf7447e2daf1ca81e889
jianyaoang <jianyaoang1@gmail.com>
2016-01-22 12:25:45 +0800

    Integrate and setup live music table view

--- Symphony/ViewController.swift
+++ Symphony/ViewController.swift
@@ -9,17 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var liveMusicTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("LiveMusicCellID")! as UITableViewCell
+        
+        cell.textLabel?.text = "Hello"
+        
+        return cell
+        
+        
+    }
+}
+
 
