298101c229642474cde79776718434e7c47d08f8
jianyaoang <jianyaoang1@gmail.com>
2016-01-22 12:16:47 +0800

    uploading project files

---
+++ Symphony/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Symphony
+//
+//  Created by Ang Jian Yao on 22/01/2016.
+//  Copyright © 2016 Ang Jian Yao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
