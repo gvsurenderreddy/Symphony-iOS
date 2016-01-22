d9be2934c1fd666442e2b36ced8250ab7a56e365
jianyaoang <jianyaoang1@gmail.com>
2016-01-22 13:27:34 +0800

    Added add review button along with functionality in ReviewsVC

--- Symphony/ReviewsViewController.swift
+++ Symphony/ReviewsViewController.swift
@@ -13,8 +13,20 @@ class ReviewsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Reviews"
+        
+        createAddreviewButtonOnNavigationItem()
     }
-
-
-
+    
+    
+    //MARK: review button
+    func createAddreviewButtonOnNavigationItem() {
+        
+        let reviewBarButton = UIBarButtonItem(title: "+", style: .Plain, target: self, action: "onAddReviewButtonPressed")
+        self.navigationItem.rightBarButtonItem = reviewBarButton
+        
+    }
+    
+    func onAddReviewButtonPressed() {
+        print("Hello")
+    }
 }
